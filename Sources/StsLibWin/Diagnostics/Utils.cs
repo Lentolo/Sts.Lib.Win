b2d9bdc0fceefdf9bf3105af98e3bc39b99750ed
Stefano Sarina <stefano.sarina@tcpos.com>
2019-01-16 14:33:40 +0100

    .

---
+++ Sources/StsLibWin/Diagnostics/Utils.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using StsLib.Diagnostics;
+using StsLib.Diagnostics.Log;
+
+namespace StsLibWin.Diagnostics
+{
+  public static class Utils
+  {
+    public static void MakeEventViewerLog(string source, LogTypes type, string text)
+    {
+      try
+      {
+        if (!string.IsNullOrEmpty(text))
+        {
+          if (string.IsNullOrEmpty(source))
+          {
+            source = "Application";
+          }
+
+          if (!EventLog.SourceExists(source))
+          {
+            try
+            {
+              EventLog.CreateEventSource(source, source);
+              var evtLog = new EventLog(source);
+              evtLog.ModifyOverflowPolicy(OverflowAction.OverwriteAsNeeded, 7);
+              evtLog.MaximumKilobytes = 2048;
+            }
+            catch
+            {
+              source = "Application";
+            }
+          }
+
+          if (type == LogTypes.Error)
+          {
+            EventLog.WriteEntry(source, text, EventLogEntryType.Error);
+          }
+          else if (type == LogTypes.Info)
+          {
+            EventLog.WriteEntry(source, text, EventLogEntryType.Information);
+          }
+          else if (type == LogTypes.Warning)
+          {
+            EventLog.WriteEntry(source, text, EventLogEntryType.Warning);
+          }
+        }
+      }
+      catch
+      {
+      }
+    }
+    public static void LogErrorToEventLog(Exception exc)
+    {
+      try
+      {
+        EventLog.WriteEntry("Application", Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location) + "\r\n\r\n" + StsLib.Diagnostics.Utils.ExtractError(exc), EventLogEntryType.Error);
+      }
+      catch
+      {
+      }
+    }
+  }
+}
