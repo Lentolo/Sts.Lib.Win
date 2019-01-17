53fa00f19f93dbe3d0ab59f10b39a519ea31d765
Stefano Sarina <stefano.sarina@tcpos.com>
2019-01-17 23:10:41 +0100

    .

--- Sources/StsLibWin/Windows/Forms/TextBox.cs
+++ Sources/StsLibWin/Windows/Forms/TextBox.cs
@@ -22,23 +22,4 @@ namespace StsLibWin.Windows.Forms
       }
     }
   }
-  public class ComboBox : System.Windows.Forms.ComboBox
-  {
-    protected override void OnGotFocus(EventArgs e)
-    {
-      base.OnGotFocus(e);
-      if (!string.IsNullOrEmpty(Text))
-      {
-        SelectAll();
-      }
-    }
-    protected override void OnKeyDown(KeyEventArgs e)
-    {
-      base.OnKeyDown(e);
-      if (!e.Alt && e.Control && !e.Shift && e.KeyCode == Keys.A)
-      {
-        SelectAll();
-      }
-    }
-  }
 }
