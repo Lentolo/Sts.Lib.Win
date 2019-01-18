c7debfa70e3fedf18b1ca897f85a0be07f8dbedd
Stefano Sarina <stefano.sarina@tcpos.com>
2019-01-18 14:53:12 +0100

    .

---
+++ Sources/StsLibWin/Diagnostics/Log/Implementations/EventLogWriter.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Diagnostics;
+using StsLib.Common.Extensions;
+using StsLib.Diagnostics.Log;
+
+namespace StsLibWin.Diagnostics.Log.Implementations
+{
+  [Serializable]
+  public class EventLogWriter : ILogWriter
+  {
+    private readonly string _source;
+    public EventLogWriter(string source)
+    {
+      _source = source;
+      if (!EventLog.SourceExists(_source))
+      {
+        EventLog.CreateEventSource(_source, _source);
+      }
+    }
+    public void WriteLog(LogTypes logType, string category, string text, object context)
+    {
+      var entryType = EventLogEntryType.Information;
+      if (logType == LogTypes.Error || logType == LogTypes.Exception)
+      {
+        entryType = EventLogEntryType.Error;
+      }
+      else if (logType == LogTypes.Warning)
+      {
+        entryType = EventLogEntryType.Warning;
+      }
+
+      foreach (var line in (category + "\r\n\r\n" + text + "\r\n\r\n" + context).BlockSplit(" '", 16000, "\r\n\r\n-- Continue --"))
+      {
+        EventLog.WriteEntry(_source, line, entryType);
+      }
+    }
+  }
+}
