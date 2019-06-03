8d1783cfb75e6bb05a8c001a1a7a0cb537063e0e
Stefano Sarina <stefano.sarina@tcpos.com>
2019-06-03 14:46:37 +0200

    TimeTracker - SetDesktopLocationWithEnsureBounds

--- Sources/StsLibWin/Windows/Forms/Form.cs
+++ Sources/StsLibWin/Windows/Forms/Form.cs
@@ -9,5 +9,13 @@ namespace StsLibWin.Windows.Forms
 {
   public class Form : System.Windows.Forms.Form
   {
+    protected void SetDesktopLocationWithEnsureBounds(int x, int y)
+    {
+      var s = Screen.FromControl(this).WorkingArea;
+      var container = new System.Drawing.Rectangle(s.X, s.Y, s.Width, s.Height);
+      var content = new System.Drawing.Rectangle(Left, Top, Width, Height);
+      var point = StsLib.Drawing.Utils.EnsureVisible(container, content);
+      SetDesktopLocation(point.X, point.Y);
+    }
   }
 }
