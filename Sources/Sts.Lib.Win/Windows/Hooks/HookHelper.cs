06aabfe6e43f204506ab9f94f5e0111f146f5bdc
Stefano <stefano@sarina.it>
2021-07-03 18:45:29 +0200

    Hooks

---
+++ Sources/Sts.Lib.Win/Windows/Hooks/HookHelper.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Sts.Lib.Win.Windows.Hooks
+{
+    public abstract class HookHelper : IDisposable
+    {
+        public enum HookTypes
+        {
+            LowLevelKeyboard = 13,
+            LowLevelMouse = 14
+        }
+
+        private IntPtr _keyboardHook = IntPtr.Zero;
+        private readonly HookProc hookProc;
+
+        public HookHelper()
+        {
+            hookProc = LowLevelKeyboardProc;
+            InstallHooks();
+        }
+
+        protected abstract HookTypes HookType
+        {
+            get;
+        }
+
+        public void Dispose()
+        {
+            UninstallHooks();
+            GC.SuppressFinalize(this);
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(HookTypes idHook, HookProc lpfn, IntPtr hMod, int dwThreadId);
+
+        [DllImport("user32.dll")]
+        private static extern int UnhookWindowsHookEx(IntPtr hHook);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr CallNextHookEx(IntPtr _, int nCode, UIntPtr wParam, IntPtr lParam);
+
+        private void UninstallHooks()
+        {
+            UnhookWindowsHookEx(_keyboardHook);
+            _keyboardHook = IntPtr.Zero;
+        }
+
+        private void InstallHooks()
+        {
+            if (_keyboardHook == IntPtr.Zero)
+            {
+                _keyboardHook = SetWindowsHookEx(HookType, hookProc, IntPtr.Zero, 0);
+            }
+        }
+
+        private IntPtr LowLevelKeyboardProc(int nCode, UIntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                OnHook(wParam, lParam);
+            }
+
+            return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+        }
+
+        protected abstract void OnHook(UIntPtr wParam, IntPtr lParam);
+
+        [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+        private delegate IntPtr HookProc(int nCode, UIntPtr wParam, IntPtr lParam);
+    }
+}
