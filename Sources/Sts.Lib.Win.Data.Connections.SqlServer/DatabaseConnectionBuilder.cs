21b4ad2912d0ed70488593f189b61d760e151b4a
Stefano <stefano@sarina.it>
2023-12-09 17:16:39 +0100

    File scoped namespaces

--- Sources/Sts.Lib.Win.Data.Connections.SqlServer/DatabaseConnectionBuilder.cs
+++ Sources/Sts.Lib.Win.Data.Connections.SqlServer/DatabaseConnectionBuilder.cs
@@ -7,51 +7,50 @@ using Sts.Lib.Data.Generic;
 using Sts.Lib.Data.Interfaces;
 using Sts.Lib.Win.Windows.Forms.Data;
 
-namespace Sts.Lib.Win.Data.Connections.SqlServer
+namespace Sts.Lib.Win.Data.Connections.SqlServer;
+
+public class DatabaseConnectionBuilder : CommonDatabaseConnectionBuilderBase
 {
-    public class DatabaseConnectionBuilder : CommonDatabaseConnectionBuilderBase
+    public override GenericConnectionString ConnectionString
     {
-        public override GenericConnectionString ConnectionString
+        get
         {
-            get
+            var builder = new SqlConnectionStringBuilder
             {
-                var builder = new SqlConnectionStringBuilder
-                {
-                    DataSource = TxtSrv.Text,
-                    MultipleActiveResultSets = true,
-                    UserID=TxtUid.Text
-                };
+                DataSource = TxtSrv.Text,
+                MultipleActiveResultSets = true,
+                UserID=TxtUid.Text
+            };
 
-                if (!string.IsNullOrEmpty(TxtPort.Text) && Common.Convert.Utils.TryParseTo(TxtPort.Text, 1433) != 1433)
-                {
-                    builder.DataSource = $"{TxtSrv.Text},{TxtPort.Text}";
-                }
-
-                if (!string.IsNullOrEmpty(TxtPwd.Text))
-                {
-                    builder.Password = TxtPwd.Text;
-                }
-
-                if (!string.IsNullOrEmpty(CmbDB.SelectedItem as string))
-                {
-                    builder.InitialCatalog = CmbDB.SelectedItem as string;
-                }
-
-                return new GenericConnectionString(typeof(SqlServerEnhancedConnection), builder.ConnectionString);
-            }
-        }
-
-        public override string DatabaseTypeName
-        {
-            get
+            if (!string.IsNullOrEmpty(TxtPort.Text) && Common.Convert.Utils.TryParseTo(TxtPort.Text, 1433) != 1433)
             {
-                return "Sql Server";
+                builder.DataSource = $"{TxtSrv.Text},{TxtPort.Text}";
             }
-        }
 
-        protected override string[] GetDatabases(IEnhancedDbConnection db)
-        {
-            return db.ExecuteReaderAndMap("SELECT NAME FROM SYS.DATABASES;", r => r["NAME"] as string).ToArray();
+            if (!string.IsNullOrEmpty(TxtPwd.Text))
+            {
+                builder.Password = TxtPwd.Text;
+            }
+
+            if (!string.IsNullOrEmpty(CmbDB.SelectedItem as string))
+            {
+                builder.InitialCatalog = CmbDB.SelectedItem as string;
+            }
+
+            return new GenericConnectionString(typeof(SqlServerEnhancedConnection), builder.ConnectionString);
         }
+    }
+
+    public override string DatabaseTypeName
+    {
+        get
+        {
+            return "Sql Server";
+        }
+    }
+
+    protected override string[] GetDatabases(IEnhancedDbConnection db)
+    {
+        return db.ExecuteReaderAndMap("SELECT NAME FROM SYS.DATABASES;", r => r["NAME"] as string).ToArray();
     }
 }
